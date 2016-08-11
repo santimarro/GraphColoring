ffbaa3c69f3e5d9b802d014274c26216a05d1ee0
Rama <ramirobarraco@gmail.com>
2016-08-11 18:00:56 -0300

    GrandeChico y ChicoGrande

--- dirmain/main.c
+++ dirmain/main.c
@@ -11,7 +11,7 @@ int main(void) {
     for(u32 i = 0; i < G->cantVertices; i++) {
         printf("%u, ", G->hashList->orden[i]->colorV);
     }
-    GrandeChico(G);
+    ChicoGrande(G);
     printf("Segundo orden: \n");
     for(u32 i = 0; i < G->cantVertices; i++) {
         printf("%u, ", G->hashList->orden[i]->colorV);
